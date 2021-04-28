f9c721116d8825d0c1773fa92adcc793c98167d2
dongtian <xiaodong.liu@wealedger.com>
2021-04-28 16:52:48 +0800

    fetch: replace base_url

--- utils/request/lib/index.js
+++ utils/request/lib/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const BASE_URL = process.env.OPPNYS_BASE_URL ? process.env.OPPNYS_BASE_URL
-  : 'http://localhost:7001';
+  : 'https://391937p1z0.picp.vip/cli';
 
 const request = axios.create({ baseURL: BASE_URL, timeout: 5000 });
 
